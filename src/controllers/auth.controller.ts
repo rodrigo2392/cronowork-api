6b2e988bda998c7b325bb89121ceddb06f26f1da
Rodrigo Méndez <91278835+rodrigo2392@users.noreply.github.com>
2024-10-31 15:17:29 -0600

    feat: login and register module

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -7,8 +7,8 @@ class AuthController {
     async login(req: express.Request, res: express.Response) {
         try {
             const {email, password} = req.body;
-            const user = await authService.login(email, password)
-            res.json({user})
+            const token = await authService.login(email, password)
+            res.json({token})
         }catch(error) {
             res.status(400).json({message: error})
         }
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import * as bodyparser from 'body-parser';
+import { rateLimit } from 'express-rate-limit'
 dotenv.config();
 import UserRoutes from './routes/user.routes';
 import AuthRoutes from './routes/auth.routes';
@@ -11,6 +12,15 @@ import { CommonRoutes } from './routes/common.routes';
 import { errorHandler } from './middlewares/error.middleware';
 
 const app = express()
+
+const limiter = rateLimit({
+	windowMs: 10 * 60 * 1000, // 15 minutes
+	limit: 100,
+	standardHeaders: 'draft-7', 
+	legacyHeaders: false,
+})
+
+app.use(limiter)
 
 app.use(cors())
 app.use(bodyparser.json())
