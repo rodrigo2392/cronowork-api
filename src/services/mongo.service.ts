6ce80e3563ed76ab35ab949694b6b2b25b4bda1f
Rodrigo Méndez <91278835+rodrigo2392@users.noreply.github.com>
2024-11-12 19:50:47 -0600

    feat: added auth to mongo

--- src/services/mongo.service.ts
+++ src/services/mongo.service.ts
@@ -1,27 +1,33 @@
-import mongoose, {ConnectOptions} from "mongoose";
-import mongoosePaginate from 'mongoose-paginate-v2'
+import mongoose, { ConnectOptions } from "mongoose";
+import mongoosePaginate from "mongoose-paginate-v2";
 
 class MongoService {
-    private options: ConnectOptions = {
-        serverSelectionTimeoutMS: 5000,
-    }
+  private options: ConnectOptions = {
+    serverSelectionTimeoutMS: 5000,
+    auth: {
+      username: process.env.MONGO_USER ?? "",
+      password: process.env.MONGO_PASSWORD ?? "",
+    },
+  };
 
-    constructor() {
-        this.connect();
-    }
+  constructor() {
+    this.connect();
+  }
 
-    getMongoose() {
-        mongoose.plugin(mongoosePaginate)
-        return mongoose;
-    }
-    connect = () => {
-        console.log("Connecting to the DB...")
-        mongoose.connect(process.env.MONGO_URI ?? "", this.options).then(() => {
-            console.log("MongoDB is connected.")
-        })
-        .catch((err) => {
-            console.log("MongoDB error:" + err)
-        })
-    }
+  getMongoose() {
+    mongoose.plugin(mongoosePaginate);
+    return mongoose;
+  }
+  connect = () => {
+    console.log("Connecting to the DB...");
+    mongoose
+      .connect(process.env.MONGO_URI ?? "", this.options)
+      .then(() => {
+        console.log("MongoDB is connected.");
+      })
+      .catch((err) => {
+        console.log("MongoDB error:" + err);
+      });
+  };
 }
 export default new MongoService();
